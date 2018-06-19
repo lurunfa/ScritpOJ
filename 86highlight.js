21207905943c58980c428cbf536452417f05b67e
dodf <1123317571@qq.com>
2018-06-19 22:18:16 +0800

    commit

--- 86highlight.js
+++ 86highlight.js
@@ -1,8 +1,14 @@
-const highlight = (...args) =>{
-    let a = args[0];
-
-    console.log(a);
+const highlight = (...args) => {
+    console.log(args)
+    let contents = args[0];
+    let inputArray = args.slice(1);
+    let str = '';
+    for (let i = 0; i < contents.length - 1; i++) {
+        str += `${contents[i]}<span class='red'>${args[i + 1]}</span>`
+    }
+    return str + contents[contents.length - 1];
 }
 const yourName = 'ScriptOJ'
 const myName = 'Jerry'
-console.log(highlight(`Hello, ${yourName}. I am ${myName}.`));
+const cc = 'asa'
+console.log(highlight`Hello, ${yourName}. I am ${myName}.`);
